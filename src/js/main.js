5dd34b67bdc419340e84875fb561331b0b2d2541
leonieschmutz <98399733+leonieschmutz@users.noreply.github.com>
2024-01-21 16:21:05 +0100

    création page focus projet

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,18 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".carousel-next",
+    prevEl: ".carousel-prev",
+  },
+});
