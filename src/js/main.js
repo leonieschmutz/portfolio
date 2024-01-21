04a29a103ba52ebe594513ca7cac7b0b45569e56
leonieschmutz <98399733+leonieschmutz@users.noreply.github.com>
2024-01-21 17:22:17 +0100

    réalisation burger menu mobile

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,14 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".carousel-prev",
   },
 });
+
+var button = document.querySelector(".header-nav-mobile-button");
+var menu = document.querySelector(".header-nav-mobile-link");
+var body = document.querySelector("body");
+var openMenu = function () {
+  menu.classList.toggle("is-open");
+  button.classList.toggle("is-active");
+  body.classList.toggle("is-blocked");
+};
+
+button.addEventListener("click", openMenu);
