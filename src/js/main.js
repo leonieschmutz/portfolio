9f29f4484f9b66b5e42c3afd5bb87626b71a24fe
leonieschmutz <98399733+leonieschmutz@users.noreply.github.com>
2024-02-04 12:47:46 +0100

    ajout animation hover

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// slider
+
 import Swiper, { Navigation, Pagination } from "swiper";
 
 Swiper.use([Navigation, Pagination]);
@@ -18,6 +20,8 @@ const swiper = new Swiper(".swiper-container", {
   },
 });
 
+// burger menu
+
 var button = document.querySelector(".header-nav-mobile-button");
 var menu = document.querySelector(".header-nav-mobile-link");
 var body = document.querySelector("body");
@@ -28,3 +32,48 @@ var openMenu = function () {
 };
 
 button.addEventListener("click", openMenu);
+
+// projet hover image
+
+document.querySelectorAll(".projets-section").forEach((section) => {
+  const hoverImageContainer = section.querySelector(".hover-image-container");
+
+  section.addEventListener("mouseenter", () =>
+    startImageFollow(section, hoverImageContainer)
+  );
+  section.addEventListener("mouseleave", () =>
+    stopImageFollow(hoverImageContainer)
+  );
+});
+
+let isFollowing = false;
+let currentHoverImageContainer = null;
+
+function startImageFollow(section, hoverImageContainer) {
+  if (isFollowing && currentHoverImageContainer !== hoverImageContainer) {
+    currentHoverImageContainer.style.display = "none";
+  }
+
+  isFollowing = true;
+  currentHoverImageContainer = hoverImageContainer;
+  hoverImageContainer.style.display = "block";
+  document.addEventListener("mousemove", (e) =>
+    updateHoverImagePosition(e, hoverImageContainer)
+  );
+}
+
+function stopImageFollow(hoverImageContainer) {
+  isFollowing = false;
+  currentHoverImageContainer.style.display = "none";
+  document.removeEventListener("mousemove", updateHoverImagePosition);
+}
+
+function updateHoverImagePosition(e, hoverImageContainer) {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  // Utilisation de requestAnimationFrame pour une animation plus fluide
+  requestAnimationFrame(() => {
+    hoverImageContainer.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+  });
+}
